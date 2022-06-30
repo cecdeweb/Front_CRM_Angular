7a51b8fbf1621aa684f6493d7b3c9ccc5fd83883
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-06-30 16:18:04 +0200

    add pages front

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginModule } from '../login/login.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
 
 
 
@@ -7,6 +11,7 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[LoginModule, IconsModule, TemplatesModule, UiModule]
 })
 export class CoreModule { }
