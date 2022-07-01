ead0e61dbb410d84cce4075b681310cdfb67515e
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-01 10:26:08 +0200

    add pages orders and page not found

---
+++ src/app/orders/pages/page-list-orders-component/page-list-orders-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageListOrdersComponentComponent } from './page-list-orders-component.component';
+
+describe('PageListOrdersComponentComponent', () => {
+  let component: PageListOrdersComponentComponent;
+  let fixture: ComponentFixture<PageListOrdersComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageListOrdersComponentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PageListOrdersComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
