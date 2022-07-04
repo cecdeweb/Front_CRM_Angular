10588217206e1966f983bb8ec2e4c157e03913e0
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-04 14:07:14 +0200

    add button and method open close nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { PageListOrdersComponentComponent } from './page-list-orders-component/page-list-orders-component.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageListOrdersComponentComponent
+    AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    NgbModule
-  ],
+  imports: [ BrowserModule, AppRoutingModule, CoreModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
