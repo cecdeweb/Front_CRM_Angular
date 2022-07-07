cdbe02d2ad05ddec693dc62f4a1e26575917ace1
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-07 15:53:58 +0200

    add icon with Font Awesome

---
+++ src/app/icons/components/icon-nav/icon-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { faNavicon, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-icon-nav',
+  templateUrl: './icon-nav.component.html',
+  styleUrls: ['./icon-nav.component.scss']
+})
+export class IconNavComponent implements OnInit {
+  public myIcon: IconDefinition;
+  constructor() { this.myIcon = faNavicon; }
+
+  ngOnInit(): void {
+  }
+
+}
