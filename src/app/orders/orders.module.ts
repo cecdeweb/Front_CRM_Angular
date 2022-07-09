e8eb3be3cd2d1dc13c002df3440d5c94548b282d
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-09 21:24:15 +0200

    add templates module and input title

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { PageListOrdersComponentComponent } from './pages/page-list-orders-component/page-list-orders-component.component';
+import { PageEditOrderComponentComponent } from './pages/page-edit-order-component/page-edit-order-component.component';
+import { PageAddOrderComponentComponent } from './pages/page-add-order-component/page-add-order-component.component';
 
 
 @NgModule({
   declarations: [
+    PageListOrdersComponentComponent,
+    PageEditOrderComponentComponent,
+    PageAddOrderComponentComponent
   ],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    SharedModule
   ]
 })
 export class OrdersModule { }
